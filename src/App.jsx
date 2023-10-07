846d615bebecc3c6e7a7e7164ca956c8885c81d9
Eduardo <128428496+EJPA0405@users.noreply.github.com>
2023-10-06 23:27:22 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import Registro from "./components/Registro";
+import Card from "react-bootstrap/Card";
+
+function App() {
+  //const [mensaje, setMensaje] = useState([]);
+
+  return (
+    <>
+      <Card>
+        <Registro />
+      </Card>
+    </>
+  );
+}
+
+export default App;
